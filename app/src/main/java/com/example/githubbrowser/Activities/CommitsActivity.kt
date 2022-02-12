5d1cc7fa33196f4792cf953454ccdb013bc9689a
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-02-12 21:17:35 +0530

    added CommitsActivity

---
+++ app/src/main/java/com/example/githubbrowser/Activities/CommitsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.githubbrowser.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubbrowser.R
+
+class CommitsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_commits)
+    }
+}
