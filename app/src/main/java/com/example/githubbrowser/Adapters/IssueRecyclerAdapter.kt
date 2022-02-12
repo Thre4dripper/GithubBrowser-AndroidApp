7070d6492278af6c4ce489f063a63c27aa3acbca
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-02-12 16:33:44 +0530

    added issue item data class and IssueRecyclerAdapter

---
+++ app/src/main/java/com/example/githubbrowser/Adapters/IssueRecyclerAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.githubbrowser.Adapters
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.githubbrowser.R
+import com.example.githubbrowser.dataModels.IssueItem
+
+class IssueRecyclerAdapter(private val issueList: MutableList<IssueItem>) :
+    RecyclerView.Adapter<IssueRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_issue_card, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val issueItem = issueList[position]
+        holder.issueText.text = issueItem.issue
+        Glide.with(holder.issuerAvatar.context).load(issueItem.imgUrl).circleCrop()
+            .into(holder.issuerAvatar)
+    }
+
+    override fun getItemCount() = issueList.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val issueText = itemView.findViewById<TextView>(R.id.issue_title_text_view)!!
+        val issuerAvatar = itemView.findViewById<ImageView>(R.id.issue_creator_avatar)!!
+    }
+}
---
+++ app/src/main/java/com/example/githubbrowser/dataModels/IssueItem.kt
@@ -0,0 +1,6 @@
+package com.example.githubbrowser.dataModels
+
+data class IssueItem(
+    val issue: String,
+    val imgUrl: String
+)
